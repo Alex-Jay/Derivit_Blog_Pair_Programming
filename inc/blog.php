0004f446093e5e8511c7b64fec7efb10fc9d0578
Alex-Jay <xhaosmaster@gmail.com>
2017-05-01 23:31:53 +0100

    Add Vote + Comment Count on Posts

--- inc/blog.php
+++ inc/blog.php
@@ -1,9 +1,32 @@
 <?php
     require_once 'database.php';
+    
     $stmt = "SELECT * FROM posts";
     $query = $db->prepare($stmt);
     $query->execute();
     $postData = $query->fetchAll();
+    
+    $amtOfComments = 0;
+    
+    function fetchCommentCount($db, $postId)
+    {
+        $stmt = "SELECT COUNT(comment_id) FROM comments WHERE post_id = $postId";
+        $query = $db->prepare($stmt);
+        $query->execute();
+        $f = $query->fetch();
+        $result = $f[0];
+        return $result;
+    }
+    
+    function fetchVoteCount($db, $postId)
+    {
+        $stmt = "SELECT COUNT(vote_id) FROM votes WHERE post_id = $postId";
+        $query = $db->prepare($stmt);
+        $query->execute();
+        $f = $query->fetch();
+        $result = $f[0];
+        return $result;
+    }
 ?>
 
 <style>
@@ -24,6 +47,7 @@
     }
 </style>
 <?php foreach($postData as $post): ?>
+    <?php $postId = $post['post_id']?>
     <div class="media">
         <div class="media-left media-top">
            <img id="post_image" class="media-object" src="img/<?php echo $post['post_image']; ?>" alt="logo">
@@ -31,6 +55,8 @@
         <div class="media-body">
             <a href="viewPost.php?=<?php echo $post['post_id']; ?>"><h4 class="media-heading"><?php echo $post['post_title']; ?></h4></a>
             <?php echo $post['post_body']; ?>
+            <p>COMMENTS: <?php echo fetchCommentCount($db, $postId) ?></p>
+            <p>VOTES: <?php echo fetchVoteCount($db, $postId) ?></p>
         </div>
     </div>
 <?php endforeach; ?>
