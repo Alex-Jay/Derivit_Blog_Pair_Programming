7cb4c5d774fb7a3442dd10f78a0ea7480a89ee45
JordanD96 <jordandowd96@yahoo.com>
2017-05-01 23:05:06 +0100

    Added a new navbar with new buttons

--- inc/profile.php
+++ inc/profile.php
@@ -1,15 +1,11 @@
 <ul class="nav navbar-nav navbar-right">
-    <a href="login.php" class="navbar-link">
-        <button type="button" class="btn btn-default navbar-btn">View Profile</button>
+    <a href="create_blog.php" class="navbar-link">
+        <button type="button" class="btn btn-default navbar-btn">Express Your Idea</button>
     </a>
     <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="caret"></span></a>
         <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
+            <li><a href="#">View Profile</a></li>
             <li role="separator" class="divider"></li>
             <li><a href="login.php">Log out</a></li>
         </ul>
