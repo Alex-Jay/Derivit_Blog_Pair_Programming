1686b052547bdc4b04880e4fa4f9f0fcd2f61191
Alex-Jay <xhaosmaster@gmail.com>
2017-05-02 00:32:16 +0100

    Added Fetch Tag in viewPost.php

--- inc/post.php
+++ inc/post.php
@@ -6,6 +6,16 @@
     $query = $db->prepare($stmt);
     $query->execute();
     $postData = $query->fetchAll();
+    
+    function fetchTag($db, $postId)
+    {
+        $stmt = "SELECT tag_name FROM posts INNER JOIN tag ON posts.tag_id = tag.tag_id WHERE post_id = $postId";
+        $query = $db->prepare($stmt);
+        $query->execute();
+        $f = $query->fetch();
+        $result = $f[0];
+        return $result;
+    }
 ?>
 <?php foreach ($postData as $post): ?>
     <div class="media">
@@ -16,6 +26,7 @@
             <h4 class="media-heading"><?php echo $post['post_title']; ?></h4>
                 <?php echo $post['post_body']; ?>
             <p>TIMESTAMP: <?php echo $post['post_timestamp'] ?></p>
+            <p>TAG: <?php echo fetchTag($db, $postId); ?></p>
         </div>
     </div>
 <?php endforeach; ?>
